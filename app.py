a7eca5fe4d7e50d8a2003fa0ce762dbd555a1d45
root <root@iZ25rhuzh6xZ.(none)>
2016-06-20 19:18:11 +0800

    refactor

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ sys.setdefaultencoding('utf-8')
 app = Flask(__name__)
 app.secret_key = 'why would I tell you my secret key?'
 
-engine = create_engine('mysql://root:123456@123.57.58.91/dailytask')
+engine = create_engine('mysql://root:123456@localhost/dailytask',connect_args={'charset':'utf8'},echo=False)
 metadata = MetaData(engine)
 Session = sessionmaker(bind=engine)
 sql_session = Session()
@@ -45,9 +45,10 @@ def validate_login():
 
         global sql_session
         user = sql_session.query(User).filter_by(username=_username).first()
-
+        
         if user and check_password_hash(user.password, _password):
             session['user'] = user.username
+            session['name'] = user.name
             return redirect('/userHome')
         else:
             return render_template('error.html', error="用户名或密码错误")
@@ -117,7 +118,6 @@ def save_report():
     status = request.form['inputStatus']
     bugs = request.form['inputBugs']
     updated_time = int(time.time())
-
     report = Report(user_id=user_id, system_name=name, status=status, bugs=bugs, updated_time=updated_time)
     sql_session.add(report)
     sql_session.commit()
@@ -128,6 +128,7 @@ def save_report():
 @app.route('/logout')
 def logout():
     session.pop('user', None)
+    session.pop('name', None)
     return redirect('/')
 
 
